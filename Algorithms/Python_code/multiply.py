3ab0b48ee1de3bd9616117696cc76d72bcd944a8
Darren Cobian <darrencobian98@gmail.com>
2022-01-13 15:38:22 -0800

    added multiplication algorithm in python

---
+++ Algorithms/Python_code/multiply.py
@@ -0,0 +1,16 @@
+import math
+
+def main():
+    return mult(3, 2)
+
+# Binary multiplication
+# Input: Two n-bit integers x and y, where y >= 0
+# Output: Their product
+def mult(x, y):
+    if y == 0: return 0
+    z = mult(x, math.floor(y/2))
+    if y % 2 == 0: return 2*z
+    else: return (x + 2*z)
+
+if __name__ == '__main__':
+    print(main())
